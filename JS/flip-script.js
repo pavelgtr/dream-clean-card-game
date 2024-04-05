e7f50c64275da6e05ca34a64b50627d73cae1df2
Pavel Rivera <pavelgtr@gmail.com>
2024-04-05 15:40:26 -0600

    working code with un-identical images

--- JS/flip-script.js
+++ JS/flip-script.js
@@ -54,25 +54,6 @@ window.onload = function () {
 };
 
 
-const newArray = [
-  // Replace the entire old array with this
-  { src: "../images/a.png", match: "../images/a copy.png" },
-  { src: "../images/b.png", match: "../images/b copy.png" },
-  { src: "../images/c.png", match: "../images/c copy.png" },
-  { src: "../images/d.png", match: "../images/d copy.png" },
-  { src: "../images/e.png", match: "../images/e copy.png" },
-  { src: "../images/f.png", match: "../images/f copy.png" },
-];
-
-// <----------------------------------------- SOUNDS -----------------------------------------> //
-const soundEffects = {
-  click: new Audio("../sounds/flip.wav"),
-  error: new Audio("../sounds/fail.wav"),
-  win: new Audio("../sounds/cheer.wav"),
-};
-
-// <----------------------------------------- VARIABLES -----------------------------------------> //
-
 let hasFlippedCard = false;
 let flippedCard1, flippedCard2;
 let gameBoardLocked = false;
@@ -81,144 +62,195 @@ let secondsElapsed = 0;
 let errorCount = 0;
 let scoreCount = 0;
 
+
 const timerDisplay = document.getElementById("timer");
 const errorDisplay = document.getElementById("errors");
-// const playButton = document.getElementById("play-button");
-const scoreDisplay = document.getElementById("score");
+const scoreDisplay = document.getElementById('score');
 
-function createCards(cardArray) {
-  // I rename to cardArray to be more descriptive
-  const container = document.querySelector(".cards-container");
-  shuffleArray(cardArray);
-  // Double the array, as now you have pairs directly in your data
-  const doubledArray = [...cardArray, ...cardArray];
 
-  container.innerHTML = doubledArray
-    .map(
-      (cardData, index) => `
-    <div class="card">
-      <div class="card__inner" onclick="flipCard(event, ${index})">
-        <div class="card__face card__face--front"><h3>?</h3></div>
-        <div class="card__face card__face--back" data-match="${cardData.match}"> 
-          <div class="card__content">
-            <div class="card__header">
-              <img src="${cardData.src}" alt="Card image ${index}" class="pp">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `
-    )
-    .join("");
-}
+const soundEffects = {
+ click: new Audio("../sounds/flip.wav"),
+ error: new Audio("../sounds/fail.wav"),
+ win: new Audio("../sounds/cheer.wav"),
+};
+
+
+
+
+
+
+const imagesArray = [
+ "../images/a.jpg",
+ "../images/a-1.jpg",
+ "../images/b.jpg",
+ "../images/b-1.jpg",
+ "../images/c.jpg",
+ "../images/c-1.jpg",
+ "../images/d.jpg",
+ "../images/d-1.jpg",
+];
+
+
+
 
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+ for (let i = array.length - 1; i > 0; i--) {
+   let j = Math.floor(Math.random() * (i + 1));
+   [array[i], array[j]] = [array[j], array[i]];
+ }
+ return array;
 }
+
+
+function createCards(imagesArray) {
+ const container = document.querySelector(".cards-container");
+ shuffleArray(imagesArray);
+ container.innerHTML = imagesArray.map((imageSrc, index) => `
+   <div class="card">
+     <div class="card__inner" onclick="flipCard(event, ${index})">
+       <div class="card__face card__face--front"><h3>?</h3></div>
+       <div class="card__face card__face--back">
+         <div class="card__content">
+           <div class="card__header">
+             <img src="${imageSrc}" alt="Card image ${index}" class="pp">
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ `).join("");
+}
+
+
+
+
+
 
 function flipCard(event, index) {
-  if (gameBoardLocked) return;
-  const card = event.currentTarget;
-  if (card === flippedCard1) return;
+ if (gameBoardLocked) return;
+ const card = event.currentTarget;
+ if (card === flippedCard1) return;
 
-  soundEffects.click.play();
-  card.classList.add("is-flipped");
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    flippedCard1 = card;
-  } else {
-    flippedCard2 = card;
-    gameBoardLocked = true;
-    checkForMatch();
-  }
+ soundEffects.click.play();
+ card.classList.add("is-flipped");
+
+
+ if (!hasFlippedCard) {
+   hasFlippedCard = true;
+   flippedCard1 = card;
+ } else {
+   flippedCard2 = card;
+   gameBoardLocked = true;
+   checkForMatch();
+ }
 }
+
 
 function checkForMatch() {
-  const isMatch =
-    flippedCard1.querySelector(".card__face--back").dataset.match ===
-    flippedCard2.querySelector(".card__face--back").dataset.match;
-  isMatch ? (disableCards(), incrementScore()) : unflipCards();
+ // Extract the base filename by removing the '.jpg' and '-1' parts
+ let baseName1 = flippedCard1.querySelector("img").src.split('/').pop().replace('.jpg', '').replace('-1', '');
+ let baseName2 = flippedCard2.querySelector("img").src.split('/').pop().replace('.jpg', '').replace('-1', '');
+  // Check if the base filenames are the same
+ const isMatch = baseName1 === baseName2;
+
+
+ if (isMatch) {
+   disableCards();
+   incrementScore();
+ } else {
+   unflipCards();
+ }
 }
+
 
 function incrementScore() {
-  scoreCount++;
-  scoreDisplay.textContent = `Score: ${scoreCount}`;
+ scoreCount++;
+ scoreDisplay.textContent = `Score: ${scoreCount}`;
 }
+
 
 function disableCards() {
-  flippedCard1.removeEventListener("click", flipCard);
-  flippedCard2.removeEventListener("click", flipCard);
-  resetTurn();
-  soundEffects.win.play();
+ flippedCard1.removeEventListener("click", flipCard);
+ flippedCard2.removeEventListener("click", flipCard);
+ resetTurn();
+ soundEffects.win.play();
 }
+
 
 function unflipCards() {
-  setTimeout(() => {
-    flippedCard1.classList.remove("is-flipped");
-    flippedCard2.classList.remove("is-flipped");
-    soundEffects.error.play();
-    incrementError();
-    resetTurn();
-  }, 1500);
+ setTimeout(() => {
+   flippedCard1.classList.remove("is-flipped");
+   flippedCard2.classList.remove("is-flipped");
+   soundEffects.error.play();
+   incrementError();
+   resetTurn();
+ }, 1500);
 }
+
 
 function resetTurn() {
-  [hasFlippedCard, gameBoardLocked] = [false, false];
-  [flippedCard1, flippedCard2] = [null, null];
+ [hasFlippedCard, gameBoardLocked] = [false, false];
+ [flippedCard1, flippedCard2] = [null, null];
 }
+
 
 function incrementError() {
-  errorCount++;
-  errorDisplay.textContent = `Errors: ${errorCount}`;
+ errorCount++;
+ errorDisplay.textContent = `Errors: ${errorCount}`;
 }
+
 
 function startTimer() {
-  timer = setInterval(() => {
-    secondsElapsed++;
-    timerDisplay.textContent = `Time: ${formatTime(secondsElapsed)}`;
-  }, 1000);
+ timer = setInterval(() => {
+   secondsElapsed++;
+   timerDisplay.textContent = `Time: ${formatTime(secondsElapsed)}`;
+ }, 1000);
 }
 
-function stopTimer() {
-  clearInterval(timer);
-  secondsElapsed = 0;
-  timerDisplay.textContent = "Time: 00:00";
-}
 
 function formatTime(seconds) {
-  const mins = Math.floor(seconds / 60);
-  const secs = seconds % 60;
-  return `${mins.toString().padStart(2, "0")}:${secs
-    .toString()
-    .padStart(2, "0")}`;
+ const mins = Math.floor(seconds / 60);
+ const secs = seconds % 60;
+ return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
 }
+
 
 function startGame() {
-  resetGame();
-  startTimer();
+ resetGame();
+ startTimer();
 
 
 }
+
 
 function stopGame() {
-  stopTimer();
-  resetGame();
+ stopTimer();
+ resetGame();
+
+
 }
 
+
 function resetGame() {
-  stopTimer();
-  errorCount = 0;
-  errorDisplay.textContent = "Errors: 0";
-  createCards(newArray);
-  resetTurn();
+ stopTimer();
+ errorCount = 0;
+ errorDisplay.textContent = "Errors: 0";
+ createCards(imagesArray);
+ resetTurn();
 }
+
+
 
 
 // Initialize the game
 document.addEventListener("DOMContentLoaded", startGame);
+
+
+
+
+function stopTimer() {
+ clearInterval(timer);
+ secondsElapsed = 0;
+ timerDisplay.textContent = "Time: 00:00";
+}
